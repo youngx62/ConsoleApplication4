7c1cc0cf922a190d7de328f346102c6caef6d099
youngx62 <xavier_tou@yahoo.com>
2016-12-15 20:08:29 -0600

    Add project files.

---
+++ ConsoleApplication4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Do you enjoy C# ? (yes/no/maybe)");
+            string input = Console.ReadLine();
+            switch (input.ToLower())
+            {
+                case "yes":
+                case "maybe":
+                    Console.WriteLine("Great!");
+                    break;
+                case "no":
+                    Console.WriteLine("Too bad!");
+                    break;
+                default:
+                    Console.WriteLine("FUck YOUUU");
+                    break;
+            }
+        }
+    }
+}
